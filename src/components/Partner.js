82b193d7dbbdd58022647b8293aa95d7a8763b31
Ted Ward <teddyward@gmail.com>
2023-08-03 16:41:48 +0100

    fix error in css

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -7,7 +7,7 @@ function Partner({name, text, image, link}) {
   return (
     <>
     <Fade delay={500} triggerOnce>
-      <div className='box-about'>
+      <div className='box'>
         <Fade delay={700} duration={2000} triggerOnce>
           <img
           src={imageURL}
